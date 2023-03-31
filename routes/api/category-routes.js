c972f39b1d98959ba90747d19fffeefec16c0653
My My <mymy@Mys-MacBook-Air.local>
2023-03-30 20:55:52 -0400

    finished category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,51 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [{
+      model: Product,
+      as: "products"
+    }]
+  }).then((result) => res.json(result));
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id, {
+    include: [{
+      model: Product,
+      as: "products"
+    }]
+  }).then((result) => res.json(result))
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  }).then((result) => res.json(result))
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    category_name: req.body.category_name
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  }).then((results) => res.json(results))
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then((results) => res.json(results))
 });
 
 module.exports = router;
